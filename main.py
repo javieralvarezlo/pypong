c5c93e8b275515db587638ba81ae38a3e8e81a67
BuildTools <unconfigured@null.spigotmc.org>
2022-11-21 17:36:11 +0100

    Add singleplayer

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 import sys
+from random import randint, uniform
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 WIDTH = 920
 HEIGHT = 720
+step = 10
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,7 +18,8 @@ box1.center = window.center
 box2 = pygame.Rect(0, 0, 30, 60)
 box2.midleft = window.midleft
 
-vec = [1, 0]
+ball_speed = [-1, 0]
+score = [0, 0]
 pygame.key.set_repeat(50, 50)
 fps = pygame.time.Clock()
 
@@ -24,9 +27,17 @@ menu = True
 singleplayer = False
 multiplayer = False
 
+left_paddle = pygame.Rect(0, 0, 15, 120)
+left_paddle.midleft = window.midleft
+right_paddle = pygame.Rect(0, 0, 15, 120)
+right_paddle.midright = window.midright
+ball = pygame.Rect(0, 0, 20, 20)
+ball.center = window.center
+
+
 
 def show_menu():
-    global menu, singleplayer, multiplayer
+    global menu, multiplayer, singleplayer
     largefont = pygame.font.Font('freesansbold.ttf', 50)
     smallfont = pygame.font.Font('freesansbold.ttf', 30)
 
@@ -51,9 +62,6 @@ def show_menu():
     screen.blit(multitext, multitext_rect)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse = pygame.mouse.get_pos()
 
@@ -63,11 +71,102 @@ def show_menu():
             if multitext_rect.collidepoint(mouse):
                 multiplayer = True
                 menu = False
+                screen.fill(black)
 
     pygame.display.flip()
 
+    
+def draw_field():
+    pygame.draw.line(screen, white, (WIDTH/2, 0), (WIDTH/2, HEIGHT))
 
+
+def draw_paddles():
+    pygame.draw.rect(screen, white, left_paddle)
+    pygame.draw.rect(screen, white, right_paddle)
+    
+
+def draw_ball():
+    pygame.draw.rect(screen, white, ball)
+    
+
+def keep_paddles():
+    if left_paddle.top < window.top:
+        left_paddle.top = window.top
+    if left_paddle.bottom > window.bottom:
+        left_paddle.bottom = window.bottom
+    if right_paddle.top < window.top:
+        right_paddle.top = window.top
+    if right_paddle.bottom > window.bottom:
+        right_paddle.bottom = window.bottom
+        
+    
+    
+def ball_collide():
+    global ball_speed
+    # top and bottom collision
+    if ball.top < window.top or ball.bottom > window.bottom:
+        ball_speed[1] = -ball_speed[1]
+        ball_speed[0] = ball_speed[0] * 1.1
+        ball_speed[1] = ball_speed[1] * 1.1
+        
+    # paddle collision
+    if ball.colliderect(left_paddle) or ball.colliderect(right_paddle):
+        ball_speed[0] = -ball_speed[0]
+        ball_speed[1] = randint(-2, 2)
+        ball_speed[0] = ball_speed[0] * 1.1
+        ball_speed[1] = ball_speed[1] * 1.1
+    
+    # right goal
+    if ball.left < window.left:
+        ball_speed = [-1, 0]
+        ball.center = window.center
+        score[1] = score[1] + 1
+        left_paddle.midleft = window.midleft
+        right_paddle.midright = window.midright
+            
+    # left goal
+    if ball.right > window.right:
+        ball_speed = [1, 0]
+        ball.center = window.center
+        score[0] = score[0] + 1
+        left_paddle.midleft = window.midleft
+        right_paddle.midright = window.midright
+        
+        
 if __name__ == "__main__":
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            
+            if not menu:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        left_paddle = left_paddle.move(0, -step)
+                    if event.key == pygame.K_DOWN:
+                        left_paddle = left_paddle.move(0, step)
+                        
+            
         if menu:
             show_menu()
+        else:
+            if singleplayer:
+                if ball_speed[1] > 0:
+                    right_paddle = right_paddle.move(0, uniform(1, ball_speed[1]))
+                elif ball_speed[1] < 0:
+                    right_paddle = right_paddle.move(0, uniform(ball_speed[1], -1))
+            if multiplayer:
+                pass
+        
+            ball = ball.move(ball_speed)
+            screen.fill(black)
+
+            draw_field()
+            draw_ball()
+            draw_paddles()
+            keep_paddles()
+            ball_collide()
+            
+            pygame.display.flip()
+            fps.tick(260)
